c710956cdc248f022ff5ddcc1da531875adb985b
Vincent Demay <vincent@demay-fr.net>
2014-09-24 21:12:47 +0200

    Full compliance with thingspeak.com

--- RF24L01Read.cpp
+++ RF24L01Read.cpp
@@ -26,7 +26,7 @@ enum TYPES {
 //DATA
 typedef struct {
   // id of the device -> max is 32 characters
-  char id[16];
+  char id[8];
   // type of the device -> a simple int
   int8_t type;
   float val[8];
